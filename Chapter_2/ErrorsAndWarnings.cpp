e3d0a897f39ac246a03ba18809a991d8a91dfd20
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-07-25 20:27:03 +0600

    Errors and Warnings

---
+++ Chapter_2/ErrorsAndWarnings.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    // Compile time error (;)
+    std::cout << "Hello World !" << std::endl;
+
+    // Run time error
+    // 7 / 0;
+
+    // int value = 7 / 0;
+    // std::cout << "Value : " << value << std::endl;
+
+    return 0;
+}
+
+/*
+
+Error and Warning
+
+01. Compile time error
+02. Run time error
+
+*/
