b984843b6db2e69938d111f4c93f08886d24eaca
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-07-25 20:05:58 +0600

    First program & comment

---
+++ Chapter_2/main.cpp
@@ -0,0 +1,25 @@
+// one line comment
+
+// This brings in the iostream library
+#include <iostream>
+
+/*
+
+    This is a block comment that englobe multiple
+    lines of text
+
+ */
+
+int main()
+{
+    // This is going to print "Hello World" console
+    std::cout << "Hello world ! " << std::endl;
+    return 0;
+    // Program ends here
+}
+
+// ------------------> comment <------------------------
+// 01. "// comment out a single line "
+// 02. "/*  */ Block comments out a block of text"
+// 03. "/*..../*....*/...*/ Block comments can't be nested"
+// Use comment to document your code.Don't overdo it though
